d9a059646fe98ad922882e54e8e1842999a39109
juan <juanpa3624@gmail.com>
2021-03-24 17:56:44 -0500

    Cambio estilos Login

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,25 +11,46 @@ export class HomeComponent implements OnInit {
   styleObj: any;
   arrModules = [
     {
-      title: 'Modulo 1',
-      description: 'Test',
+      title: 'Fases de Pruebas',
+      description: 'Definición de cada una de las fases.',
       bgc: `#3f51b5`,
       progress: 60,
-      position:{ top: 200, left: 300 }
+      position:{ top: 350, left: 300 }
     },
     {
       title: 'Modulo 2',
       description: 'Test',
       bgc: `#3f51b5`,
       progress: 20,
-      position:{ top: 200, left: 500 }
+      position:{ top: 200, left: 400 }
     },
     {
       title: 'Modulo 3',
       description: 'Test',
       bgc: `#3f51b5`,
-      progress: 5,
-      position:{ top: 200, left: 700 }
+      progress: 50,
+      position:{ top: 100, left: 550 }
+    },
+    {
+      title: 'Modulo 4',
+      description: 'Test',
+      bgc: `#3f51b5`,
+      progress: 10,
+      position:{ top: 100, left: 750 }
+    },
+    {
+      title: 'Modulo 5',
+      description: 'Test',
+      bgc: `#3f51b5`,
+      progress: 100,
+      position:{ top: 200, left: 900 }
+    },
+    {
+      title: 'Modulo 6',
+      description: 'Test',
+      bgc: `#3f51b5`,
+      progress: 80,
+      position:{ top: 350, left: 1000 }
     },
   ]
 
