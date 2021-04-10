26f567ec9ec30ffbca7e69e7e35bb05e0aa1e514
Christian Diaz <christiancadiaz@gmail.com>
2021-04-09 22:43:03 -0500

    implementacion de alertas y ajuste de catch en registro

--- src/app/components/login/signup/signup.component.ts
+++ src/app/components/login/signup/signup.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { UtilService } from '../../../services/util.service';
 import { AuthService } from '../../../services/auth.service';
+import { CONFIG_ALERT, ERR_AUTH, TAGS } from '../../../app.constants';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'signup',
@@ -12,29 +15,25 @@ export class SignupComponent implements OnInit {
   registerForm = new FormGroup({
     displayName: new FormControl(''),
     photo: new FormControl(''),
-    email: new FormControl(''),
+    email: new FormControl('test'),
     password: new FormControl('')
   })
-  constructor(public auth: AuthService) { 
-    
-  }
+  constructor(public auth: AuthService, private utilService: UtilService, private router: Router) { }
 
-  ngOnInit(): void {
-    console.log('user', this.auth.getUser());
-    
-    
-  }
+  ngOnInit(): void {}
 
   onRegister(){
-    this.auth.register(this.registerForm.value).then((res)=>{
-      
-    })
-    /* this.auth.register(this.registerForm.value).then(
-      (user: any)=>{
-        console.log(user);
+    this.auth.register(this.registerForm.value).then(()=>{
+      this.utilService.openCustomAlert(TAGS.LABELS.COMPLETE_REGISTER,'success',CONFIG_ALERT.TIMERS.DEFAULT,'top-end');
+      this.auth.login(this.registerForm.value).then(()=>{
+        this.router.navigate(['/dashboard'])
+      })
+    }).catch((err)=>{
+      for(const index in ERR_AUTH){
+        if(ERR_AUTH[index]['CODE'] == err.code)
+          this.utilService.openCustomAlert(TAGS.LABELS.ERROR,'error',CONFIG_ALERT.TIMERS.DEFAULT,'top-end',ERR_AUTH[index]['MESSAGE']);
       }
-    ); */
-
+    })
   }
   
 }
---
+++ src/app/models/alert.model.ts
@@ -0,0 +1,5 @@
+import { SweetAlertOptions } from 'sweetalert2'
+export class Alert {
+    title : string;
+    options: SweetAlertOptions;
+}
